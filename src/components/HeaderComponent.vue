97f55e0849e46dadf4ff690cee59263c242b3023
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-05-16 14:04:46 +0200

    HeaderComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,65 @@
 <template>
     <header>
-        HEADER
+        <div class="container d-flex justify-content-between">
+            <img src="../assets/images/headerImg/logo.png" alt="logo">
+            <ul>
+                <li v-for="(item, index) in datiHeader" :key="index" :class="{'selected': item.active}">
+                    <img :src="item.active ? item.iconSelect : item.icon" alt="">
+                    <a :href="item.link" :class="{'selected': item.active}">{{item.nome}}</a>
+                </li>
+            </ul>
+        </div>
     </header>
 </template>
 
 <script>
+import dati from '../dati'
 export default {
-    name: 'HeaderComponent'
+    name: 'HeaderComponent',
+    data(){
+        return {
+            datiHeader: dati,
+        }
+    },
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '../style/vars';
+header{
+    
+    img {
+        width: 130px;
+    }
+    ul{
+        list-style: none;
+        display: flex;
+        margin-bottom: 0;
+        gap: 0.5rem;
 
+        li{
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            padding: 0 0.8rem;
+            gap: 0.5rem;
+            font-size: 0.8rem;
+            cursor: pointer;
+            &.selected, &:hover{
+                background-color: $orangeColor;
+            }
+            img{
+                width: 25px;
+            }
+            a{
+                color: $blueColor;
+                font-weight: bold;
+                &.selected{
+                    color: white;
+                }
+            }
+        }
+    }
+}
 </style>
---
+++ src/dati.js
@@ -0,0 +1,53 @@
+const dati = [
+    {
+        icon: require("./assets/images/headerImg/home.png"),
+        iconSelect: require("./assets/images/headerImg/home-2.png"),
+        nome: 'Home',
+        link: '#',
+        active: true,
+    },
+    {
+        icon: require("./assets/images/headerImg/document.png"),
+        iconSelect: require("./assets/images/headerImg/document-2.png"),
+        nome: 'Pages',
+        link: '#',
+        active: false,
+    },
+    {
+        icon: require("./assets/images/headerImg/printer.png"),
+        iconSelect: require("./assets/images/headerImg/printer-2.png"),
+        nome: 'Blog',
+        link: '#',
+        active: false,
+    },
+    {
+        icon: require("./assets/images/headerImg/cart.png"),
+        iconSelect: require("./assets/images/headerImg/cart-2.png"),
+        nome: 'Shop',
+        link: '#',
+        active: false,
+    },
+    {
+        icon: require("./assets/images/headerImg/lab.png"),
+        iconSelect: require("./assets/images/headerImg/lab-2.png"),
+        nome: 'Shortcodes',
+        link: '#',
+        active: false,
+    },
+    {
+        icon: require("./assets/images/headerImg/chat.png"),
+        iconSelect: require("./assets/images/headerImg/chat-2.png"),
+        nome: 'Support',
+        link: '#',
+        active: false,
+    },
+    {
+        icon: require("./assets/images/headerImg/envelope.png"),
+        iconSelect: require("./assets/images/headerImg/envelope-2.png"),
+        nome: 'Contact',
+        link: '#',
+        active: false,
+    },
+]
+
+export default dati
